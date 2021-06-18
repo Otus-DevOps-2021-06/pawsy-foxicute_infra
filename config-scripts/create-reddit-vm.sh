d2b7a74352dd1f381b0b3df135721fcafef1bb5f
Denis Yusupov <wily.mister.fox@gmail.com>
2021-06-18 16:55:09 +0000

    fix script again

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,6 +1,6 @@
 yc compute instance create \
   --name reddit-full-vm \
   --zone ru-central1-a \
-  --create-boot-disk name=disk1,size=15,image-id=fd892l6aqsdcg3m833t2 \
+  --create-boot-disk name=disk1,size=15,image-id=fd8uvu4ge01f07f3vfsg \
   --public-ip \
   --ssh-key ~/.ssh/appuser.pub
