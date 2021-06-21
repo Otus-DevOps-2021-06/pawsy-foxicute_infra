3f3154aab3fdeec5f3b04358cee4048117019bf6
Denis Yusupov <wily.mister.fox@gmail.com>
2021-06-21 18:27:19 +0000

    fix

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  # token     = var.token
+  # version                  = "~> 0.35.0"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
