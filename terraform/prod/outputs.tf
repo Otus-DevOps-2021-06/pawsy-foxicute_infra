768075ff6ccea8e77d9aece3643955a86121aa79
Denis Yusupov <wily.mister.fox@gmail.com>
2021-06-21 18:02:27 +0000

    fix

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -1,8 +1,4 @@
-#output "external_ip_address_app" {
-#value = yandex_compute_instance.app.network_interface.0.nat_ip_address
-#  value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
-#}
-output "external_ip_address_app" {
+oputput "external_ip_address_app" {
   value = module.app.external_ip_address_app
 }
 output "external_ip_address_db" {
