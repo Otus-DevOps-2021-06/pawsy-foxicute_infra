6d25e4fe13c5237a129e0dc867f0d3c33e0c9511
Denis Yusupov <wily.mister.fox@gmail.com>
2021-06-22 12:53:11 +0000

    from stage to prod

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -5,11 +5,12 @@ provider "yandex" {
   zone                     = var.zone
 }
 module "vpc" {
-  source          = "../modules/vpc"
-  v4_blocks       = var.in_v4_blocks
+  source    = "../modules/vpc"
+  v4_blocks = var.in_v4_blocks
 }
 module "app" {
   source          = "../modules/app"
+  int_db_address  = module.db.internal_ip_address_db
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
